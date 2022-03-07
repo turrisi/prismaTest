28717f6034fbe3db32804671022f9ebe264c2078
jose1up <josealcaraz022@gmail.com>
2022-03-06 22:15:04 -0300

    tablas product / category

--- src/init.ts
+++ src/init.ts
@@ -1,40 +1,67 @@
-import { PrismaClient } from "@prisma/client"
-import axios, { AxiosResponse } from "axios"
-const prisma = new PrismaClient()
+import { PrismaClient, Prisma } from "@prisma/client";
+const prisma = new PrismaClient();
+import axios, { AxiosResponse } from "axios";
 
-const requestCategory = async () =>{
-    const {data}:any = await axios.get("https://the-funko-api.herokuapp.com//api/v1/categories").then((response:AxiosResponse)=> response.data)
-    return data
-}
+const requestCategory = async () => {
+  const { data }: any = await axios
+    .get("https://the-funko-api.herokuapp.com//api/v1/categories")
+    .then((response: AxiosResponse) => response.data);
 
+  return data;
+};
 
+const requestProduct = async () => {
+  let array: any[] = [];
+  for (let i = 1; i <= 3; i++) {
+    const { data }: any = await axios
+      .get(`https://the-funko-api.herokuapp.com//api/v1/items/?page=${i}`)
+      .then((response: AxiosResponse) => response.data);
+    array.push(data);
+  }
+  let array2 = array.flat();
 
-const requestProduct = async () =>{
-    for(let i = 1; i<= 2; i++){
-        const {data}:any = await axios.get(`https://the-funko-api.herokuapp.com//api/v1/items/?page=${i}`).then((response:AxiosResponse)=> response.data)
-        return data
-    }
-}
+  return array2;
+};
 
-export const createProduct = async ()=>{
-    const {data}:any = await axios.get("https://the-funko-api.herokuapp.com//api/v1/categories").then((response:AxiosResponse)=> response.data)
-}
+// export const createProduct = async () => {
+//   const { data }: any = await axios
+//     .get("https://the-funko-api.herokuapp.com//api/v1/categories")
+//     .then((response: AxiosResponse) => response.data);
+//     return data;
+// };
 
-export const findCategory = (pro:[], name:String)=>{
-    let result = pro.find((elemento:any) => elemento.attributes.name === name)
-    return result
-}
+// export const findCategory = (pro: [], name: String) => {
+//   let result = pro.find((elemento: any) => elemento.attributes.name === name);
+//   return result;
+// };
 
-export const init = async () =>{
-    try{
-       let allProduct = await requestProduct()
-       let allCategory = await requestCategory()
-       allProduct.map(async (elemento:any)=> {
-           let category = findCategory(allCategory, elemento.attributes.category)
-           
-          
-        })
-    }catch(error){
-        console.log(error)
-    }
-}
+export const init = async () => {
+  try {
+    const allProducts = await requestProduct();
+
+    allProducts.map(async (product: any) => {
+      await prisma.product.create({
+        data: {
+          title: product.attributes.title,
+          number: product.attributes.number,
+          Category: {
+            connectOrCreate: {
+              create: { name: product.attributes.category },
+              where: {
+                name: product.attributes.category,
+              },
+            },
+          },
+          Brand: {
+            create: { name: product.attributes.brand },
+          },
+          License: {
+            create: { name: product.attributes.license },
+          },
+        },
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
